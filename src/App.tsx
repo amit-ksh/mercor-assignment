375e38b8abe180e0b0be0f2c03d5150ca3a2a9ce
amit-ksh <ksamit1110@gmail.com>
2023-05-14 14:09:15 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Section1 from './components/Section1'
+import Section2 from './components/Section2'
+
+function App() {
+
+  return (
+    <>
+      <div style={{height: '50vh'}}></div>
+      < hr />
+      <Section1 />
+      < hr />
+      <Section2 />
+      < hr />
+      <div style={{height: '50vh'}}></div>
+    </>
+  )
+}
+
+export default App
